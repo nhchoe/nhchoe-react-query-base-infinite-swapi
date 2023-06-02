e29c5b1add16c58e80ca9053b806c788e52eb82d
nhchoe <nhchoe.dev@gmail.com>
2023-06-02 21:28:26 +0900

    Create query client and add provider to App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import "./App.css";
-import { InfinitePeople } from "./people/InfinitePeople";
-import { InfiniteSpecies } from "./species/InfiniteSpecies";
+import './App.css';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+import { InfinitePeople } from './people/InfinitePeople';
+import { InfiniteSpecies } from './species/InfiniteSpecies';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="App">
-      <h1>Infinite SWAPI</h1>
-      <InfinitePeople />
-      {/* <InfiniteSpecies /> */}
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className='App'>
+        <h1>Infinite SWAPI</h1>
+        <InfinitePeople />
+        {/* <InfiniteSpecies /> */}
+      </div>
+    </QueryClientProvider>
   );
 }
 
